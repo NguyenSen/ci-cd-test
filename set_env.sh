a77f183d11cc6f27cd03a05da3f977bf5cf9de17
NguyenSen <Nguyendinhsen@gmail.com>
2022-11-27 12:55:26 +0700

    -a

---
+++ set_env.sh
@@ -0,0 +1,18 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.cw05phfqjsdj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-tuannd
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export AWS_ACCESS_KEY_ID=AKIAQVSOKBVZDHTASDJJ
+export AWS_SECRET_ACCESS_KEY=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
+export URL=http://localhost:8080
+
+
+
